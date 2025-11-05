e34b91372ce900e18a6b46ad39d38d7b6a9105e6
Pragati damkondwar <pragatidamkondwar952@gmail.com>
2025-11-05 08:58:58 +0530

    Add files via upload

---
+++ core Java/Assignment/31-10-2025/Distance.java
@@ -0,0 +1,28 @@
+
+class Distance
+{
+int Km,M;
+void setKm(int x)
+{
+this.Km=x;
+}
+void setM(int y)
+{
+this.M=y;
+}
+//setter ends here
+     
+}//class Distance ends here
+
+class DistanceTest
+{
+public static void main (String [] args)
+{
+Distance d1;
+d1=new Distance();
+d1.setKm(12);
+d1.setM(500);
+
+System.out.println("km is:"+d1.Km+"/"+"m is:"+d1.M);
+}
+}//class Distance Test end here
