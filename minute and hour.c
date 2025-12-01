921344731821b9787f4cd7955afda2a79bf4a302
Pragati damkondwar <pragatidamkondwar952@gmail.com>
2025-12-01 10:59:28 +0530

    Delete minute and hour.c

--- minute and hour.c
+++
@@ -1,9 +0,0 @@
-//convert minutes into hours and remaining minute//
-void main()
-{
-	int totalmin = 80 ,  hours, min ;
-	hours= totalmin/60;
-	min= totalmin%60;
-	printf("convert hour %d",hours);
-    printf("convert minute %d",min);
- } 
