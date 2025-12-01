71bb15bf53a4b92a4ccab3bac45adb7ce6845a55
Pragati damkondwar <pragatidamkondwar952@gmail.com>
2025-12-01 11:02:21 +0530

    Delete avg.c

--- avg.c
+++
@@ -1,9 +0,0 @@
-//Enter five number and find average//
-void main()
-{
-	 int a = 2, b=3 , c=4 ,d=5 , e=6 , sum , avg;
-	 sum=a + b + c + d + e ;
-	 avg=sum / 5;
-	 printf("enter five number %d",avg);
-	 
-}
